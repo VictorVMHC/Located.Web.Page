d87947ae1c7d597cf60c2889e0eea1298b4f7bbd
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-11-07 21:15:01 -0600

    Added more things

--- src/Components/DownLoadButton.tsx
+++ src/Components/DownLoadButton.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const DownloadButton: React.FC = () => {
+
+    const downloadImage = () => {
+        console.log("hola");
+        
+            const imagenURL = "./public/manager.apk";
+            console.log(imagenURL);
+            const enlace = document.createElement('a');
+            enlace.href = imagenURL;
+            enlace.download = 'manager.apk';
+            enlace.click();
+    }
     return (
-        <a href="..\assets\react.svg" download >
-            <button>DownLoad</button>
+
+
+        <a download >
+            <button onClick={() => downloadImage() }>DownLoad</button>
         </a>
     );
 };
