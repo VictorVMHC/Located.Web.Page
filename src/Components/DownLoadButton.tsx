ec7fd971fcaa9950f05b1d1c5fcbd135825b0486
Victor Hernandez <73491889+VictorVMHC@users.noreply.github.com>
2023-11-14 23:04:41 -0600

    updated apk

--- src/Components/DownLoadButton.tsx
+++ src/Components/DownLoadButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DownloadButton: React.FC = () => {
 
     const downloadImage = () => {
-            const imagenURL = "./public/LocatedApp.apk";
+            const imagenURL = "../assets/apks/LocatedApp.apk"
             const enlace = document.createElement('a');
             enlace.href = imagenURL;
             enlace.download = 'LocatedApp.apk';
